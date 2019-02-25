6cf46c69774f80254c3676bdf06cde277104ebc7
Mitch Vance <vancemi@uwplatt.edu>
2019-02-25 17:46:52 -0600

    MessageService Template.

---
+++ Program/Mock UI/MessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock_UI
+{
+    class MessageService : IMessageService
+    {
+        public bool SendMessage(string message)
+        {
+            return true;
+        }
+    }
+}
