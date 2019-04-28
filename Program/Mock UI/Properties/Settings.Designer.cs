a99db015776c2dcb520350b704f3d8e5762e8206
vancemi <vancemi@uwplatt.edu>
2019-04-28 17:52:07 -0500

    Changed settings

--- Program/Mock UI/Properties/Settings.Designer.cs
+++ Program/Mock UI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Mock_UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -50,18 +50,6 @@ namespace Mock_UI.Properties {
         public string light_grey {
             get {
                 return ((string)(this["light_grey"]));
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
-        public int timer_duration {
-            get {
-                return ((int)(this["timer_duration"]));
-            }
-            set {
-                this["timer_duration"] = value;
             }
         }
     }
